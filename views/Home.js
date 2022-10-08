976ad4b2201752d29aea6fb54f960be9ff7be961
jngnandan <jngnandan@gmail.com>
2022-10-09 00:35:27 +0100

    page added

--- views/Home.js
+++ views/Home.js
@@ -1,47 +1,34 @@
-import { StyleSheet, Text, View, Screen, ScrollView, Image } from 'react-native'
+import { StyleSheet, Text, View, Screen, ScrollView, Image, ImageBackground } from 'react-native'
 import React from 'react'
 
-export default function Home() {
-  return (
-    <View style={{height: '100%', }}>
-    <View style={styles.centered}>
-      <Text style={{fontSize: 30, fontWeight: '700', color: 'white', paddingBottom: 14,}}>Super Over League</Text>
-      <View style={{flexDirection: 'row'}}>
-        <Image
-        style={styles.logo}
-        source={{
-          uri: 'https://assets.ccbp.in/frontend/react-js/rcb-img.png',
-        }}
-      />
-      <Image
-        style={styles.logo}
-        source={{
-          uri: 'https://assets.ccbp.in/frontend/react-js/csk-img.png',
-        }}
-      />
-      
+import {screen} from '../../simpleApp/images/screen.png'
 
-      </View>
-    </View>
+export default function Home() {
+  const img = {uri: "https://assets.ccbp.in/frontend/react-js/congrats-card-bg.png"}
+
+  return (
+    <View style={{height: '100%'}}>  
+      <ImageBackground resizeMode="cover" source={img} style={styles.image}>
+        <Text style={{fontSize: 36, fontWeight: '400', paddingBottom: 14}}>Congratualtions</Text>
+        <View style={{backgroundColor: '#cffafe', width: '100%', height: 'auto', borderRadius: 6, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingVertical: 20}}>
+            <Image source={{uri: "https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"}} style={{width: 100, height: 100}} />
+            <Text style={{fontSize: 24, fontWeight: '400', marginTop: 10}}>Kiran V</Text>
+            <Text style={{fontSize: 16, fontWeight: '400', marginTop: 10, textAlign: 'center'}}>You have successfully completed the React Native Basics Course</Text>
+            <Image source={{uri: "https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"}} style={{width: 100, height: 100, marginTop: 20}} />
+
+        </View>
+              <Text>Hello</Text>
+      </ImageBackground>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  centered: {
-    flex: 2, 
+  image: {
+    flex: 1,
+    // resizeMode: "cover",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#0f172a",
-    // alignSelf: 'center',
-    // height: '50%',
-    // borderRadius: 10,
-    // borderWidth: 1,
-    // borderColor: '#fff'
+    marginHorizontal: 20,
   },
-  logo: {
-    width: 180,
-    height: 200,
-  }
-
 })
